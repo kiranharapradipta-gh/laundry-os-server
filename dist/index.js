1f92f927fa968d3ac6a59be74e34635368517d8a
Kiran Hara Pradipta <kiranharapradipta@gmail.com>
2026-09-11 21:49:38 +0700

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.get("/api/health", (_req, res) => {
+    res.json({
+        success: true,
+        message: "Laundry Backend is running",
+    });
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`API running at http://localhost:${PORT}`);
+});
+//# sourceMappingURL=index.js.map
